a28fa97e7f8f30f09eb75250fe0adb321125c4e0
Garrett-Thomas <league@WTS5.attlocal.net>
2017-12-16 11:33:17 -0800

    sleepy head and change calculator

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -1,6 +1,6 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,10 +8,13 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
 
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
+	// 2. Make a String variable to hold the value of Pi. You could use
+	// http://www.piday.org/million/ for the value.
 
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second
+	// is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
@@ -20,7 +23,8 @@ public class PiAloud {
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+	// *8. If they are correct, print out "correct". If they are not, print
+	// "incorrect" to System.err.println
 
 	static void speak(char characterToSpeak) {
 		try {
@@ -38,12 +42,9 @@ public class PiAloud {
 		}
 	}
 
-
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
 
 }
-
-
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,36 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
-
-
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
-}
+		/*
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		 */
+		int yesno = JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		int noyes = JOptionPane.showConfirmDialog(null, "is it vacation?", "", JOptionPane.YES_NO_OPTION);
+		System.out.println(noyes);
+		System.out.println(yesno);
+		if (noyes == 1) {
+			JOptionPane.showMessageDialog(null, "sleep in");}
+		else if (noyes==0) {
+			JOptionPane.showMessageDialog(null, "WAKE UP! Your late!");
+		}
+		if (yesno==1) {
+			JOptionPane.showMessageDialog(null, "sleep in");}
+		}
+		
+		else if (yesno == 0) {
+			JOptionPane.showMessageDialog(null, "WAKE UP! Your late!");
+		}
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+	}}
